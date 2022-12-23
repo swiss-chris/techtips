4bc71ab8f50b0a31ff7b5ae448a1fa4971cbded4
ue70941 <chris.dickinson@gmx.ch>
2022-12-23 16:32:54 +0100

    implement a counter cycling through 1-100

--- examples/spring-integration-4.1-websockets-example/src/main/java/demo/Application.java
+++ examples/spring-integration-4.1-websockets-example/src/main/java/demo/Application.java
@@ -2,19 +2,30 @@ package demo ;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.websocket.ServerWebSocketContainer;
 import org.springframework.integration.websocket.outbound.WebSocketOutboundMessageHandler;
-import org.springframework.messaging.*;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.Executors;
-import java.util.function.Function;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 /**
  * @author Artem Bilan
@@ -26,6 +37,8 @@ import java.util.stream.Collectors;
 @RestController
 public class Application {
 
+    private static final GenericMessage<Object> EMPTY_MESSAGE = new GenericMessage<>("");
+    private static final AtomicInteger counter = new AtomicInteger();
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -47,23 +60,32 @@ public class Application {
 
     @Bean
     IntegrationFlow webSocketFlow() {
-        return f -> {
-            Function<Message, Object> splitter = m -> serverWebSocketContainer()
-                    .getSessions()
-                    .keySet()
-                    .stream()
-                    .map(s -> MessageBuilder.fromMessage(m)
-                            .setHeader(SimpMessageHeaderAccessor.SESSION_ID_HEADER, s)
-                            .build())
-                    .collect(Collectors.toList());
-            f.split( Message.class, splitter)
-                    .channel(c -> c.executor(Executors.newCachedThreadPool()))
-                    .handle(webSocketOutboundAdapter());
-        };
+        return flow -> flow
+                .log(Message::getPayload)
+                .split(Message.class, m -> serverWebSocketContainer()
+                        .getSessions()
+                        .keySet()
+                        .stream()
+                        .map(s -> MessageBuilder.fromMessage(m)
+                                .setHeader(SimpMessageHeaderAccessor.SESSION_ID_HEADER, s)
+                                .build())
+                        .collect(Collectors.toList()))
+                .channel(c -> c.executor(Executors.newCachedThreadPool()))
+                .handle(webSocketOutboundAdapter());
     }
 
     @RequestMapping("/hi/{name}")
     public void send(@PathVariable String name) {
         requestChannel().send(MessageBuilder.withPayload(name).build());
+    }
+
+    @Bean
+    IntegrationFlow isochroneSchedulerFlow() {
+        var nbIncrementsPerSecond = 20;
+        return IntegrationFlows
+                .from(() -> EMPTY_MESSAGE, e -> e.poller(Pollers.fixedRate(1000 / nbIncrementsPerSecond, MILLISECONDS)))
+                .transform(m -> counter.incrementAndGet() % 100)
+                .channel(webSocketFlow().getInputChannel())
+                .get();
     }
 }
